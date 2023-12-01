9105c57a141f8f88778d69e5618cef03aaf056cb
Yuriel849 <yuriel849sp@gmail.com>
2023-12-01 15:25:05 +0900

    Add Juhee's code

--- VisionApp/ImageProcs/Main.cpp
+++ VisionApp/ImageProcs/Main.cpp
@@ -15,6 +15,7 @@ std::string LOCATION_GREEN = "./res/img/Location_green.bmp";
 std::string EDGE_LOCATION_NAVY = "./res/img/Edge_location_navy.bmp";
 std::string DONUT_NAVY = "./res/img/Donut_navy.bmp";
 
+void Wafer_color(string& filePath);
 void QR_Detect(cv::Mat& draw_img, cv::Mat& fill_img);
 
 void main()
@@ -25,6 +26,10 @@ void main()
 
 	cv::Mat src = cv::imread(DONUT_NAVY, cv::ImreadModes::IMREAD_GRAYSCALE);
 	cv::Mat drawing = cv::imread(DONUT_NAVY, cv::ImreadModes::IMREAD_ANYCOLOR);
+
+	Wafer_color(DONUT_NAVY);		//색샹 검출 함수 호출
+
+	QR_Detect(drawing, drawing);		//QR 검출 함수 호출
 
 	//InspectScratch* pins = &insp_scratch;
 	//InspGeneric* pins = &insp_scratch;
@@ -42,8 +47,6 @@ void main()
 	vVRegions.push_back(&Scratch_Regions);
 	vVRegions.push_back(&Crack_Regions);
 	vVRegions.push_back(&Cont_Regions);
-	
-	QR_Detect(drawing, drawing);
 
 	for (size_t i = 0; i < vInsps.size(); i++)
 	{
@@ -51,7 +54,6 @@ void main()
 		std::vector<cv::Rect>* vRegions = vVRegions[i];
 
 		pins->OnTestProcess(src, drawing, vRegions);
-
 	}
 
 
@@ -60,28 +62,44 @@ void main()
 	cv::resizeWindow("Wafer Defect Detection", 1200, 800);
 	imshow("Wafer Defect Detection", drawing);
 	waitKey();
+}
 
+void Wafer_color(string& filePath)		//wafer 색상 검출할 이미지
+{
+	cv::Mat rgb_image = cv::imread(filePath, cv::IMREAD_COLOR);
+
+	cv::Mat hsv_image;
+	cv::cvtColor(rgb_image, hsv_image, cv::COLOR_BGR2HSV);		// RGB -> HSV 변환
+
+	cv::Scalar mean_hsv = cv::mean(hsv_image);
+
+	double average_hue = mean_hsv[0];
+	std::cout << "hue 값: " << average_hue << std::endl;
+
+	Point ptTxt = Point(50, 80);
+	string msg = std::format("Hue = {:.1f}", average_hue);
+	putText(hsv_image, msg, Point(ptTxt.x, ptTxt.y), FONT_HERSHEY_SIMPLEX, 2, CV_RGB(0, 255, 255), 4, 8);
+
+	int stop = 0;
 }
 
 void QR_Detect(cv::Mat& draw_img, cv::Mat& fill_img)
 {
-	// draw_img       QR 검출할 이미지 IMREAD_ANYCOLOR
-	// fill_img        QR 검출할 이미지 IMREAD_ANYCOLOR
+	// draw_img		 QR 검출할 이미지 IMREAD_ANYCOLOR
+	// fill_img	     QR 검출할 이미지 IMREAD_ANYCOLOR
 
 	QRCodeDetector QRdetector;
 
-	vector<Point> QRpoints;
-	String QRinfo = QRdetector.detectAndDecode(draw_img, QRpoints);
-	std::cout << "here" << std::endl;
+	vector<Point> QR_points;
+	String QR_info = QRdetector.detectAndDecode(draw_img, QR_points);
 
-	if (!QRinfo.empty())
+	if (!QR_info.empty())
 	{
-		std::cout << "not empty" << std::endl;
 		// polylines(src_QR_test, points, true, Scalar(0, 0, 255), 2);
 		// putText(src_QR_test, info, Point(10, 30), FONT_HERSHEY_DUPLEX, 1, Scalar(0, 0, 255));
-		polylines(draw_img, QRpoints, true, Scalar(0, 0, 255), 3);
-		putText(draw_img, QRinfo, QRpoints[0], FONT_HERSHEY_SIMPLEX, 0.8, Scalar(0, 0, 255));
-		polylines(fill_img, QRpoints, true, Scalar(0, 0, 255), 3);
-		putText(fill_img, QRinfo, QRpoints[0], FONT_HERSHEY_SIMPLEX, 0.8, Scalar(0, 0, 255));
+		polylines(draw_img, QR_points, true, Scalar(0, 0, 255), 3);
+		putText(draw_img, QR_info, QR_points[0], FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255));
+		polylines(fill_img, QR_points, true, Scalar(0, 0, 255), 3);
+		putText(fill_img, QR_info, QR_points[0], FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255));
 	}
 }
